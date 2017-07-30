512541d38efe97e6e62e733dd1a1585988fd3077
rogerdavidvera <rovera@vassar.edu>
2017-07-30 19:32:14 -0400

    simplified site

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-
 /* Request a GIF from GIPHY API, and append to HTML element*/
 function fetchGif() {
 	$('#search').text('Get another otter');
-	$.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?q="+otterSelect()+"&api_key=242b2536eefb4eb5961d2af4c122acbc",
-    success: function(results){
-    	var randomIndex = Math.floor(Math.random() * 25);
-        var data = results.data[randomIndex];
-        var gif = data.images.original.url;
-        $('#pic').remove();
-        $("#gif").append("<img id='pic' src='" + gif + "''>");
-        console.log(results);
-        $("#pic").css( "width", "60%" );
-    }
-});
-}
 
-/* Using a random number from 0 to 4, select one of four otter options*/
-function otterSelect() {
-	var randomNum = Math.floor(Math.random() * 5);
-	var string = "";
-	switch(randomNum) {
-		case 0:
-			string = "river+otter";
-			break;
-		case 1:
-			string = "river+otters";
-			break;
-		case 2:
-			string = "otter";
-			break;
-		case 3:
-			string = "otters";
-			break;
-		case 4:
-			string = "cute+otter";
-			break;
-	}
-	return string;
-}
+	$.ajax(
+	{
+		// GETS an array of 25 GIF objects that match 
+		// the search term "otter" (along with two other, non-important objects)
+
+    	url: "https://api.giphy.com/v1/gifs/search?q="+ "otter"
+    		+ "&api_key=242b2536eefb4eb5961d2af4c122acbc",
+
+    	success: function(results){
+
+    		// Select a random number from 0 to 25
+    		var randomIndex = Math.floor(Math.random() * 25);
+
+    		// Use that random number to get a random GIF object
+        	var gifObject = results.data[randomIndex];
+
+			// Get URL of original unaltered GIF
+        	var gifURL = gifObject.images.original.url;
+
+        	// Remove existing element with ID #gif (the first time there is none)
+        	$('#gif').remove();
+
+        	// Append new img element (with id #gif)
+        	$("#gifSection").append("<img id='gif' src='" + gifURL + "''>");
+
+        	//$("#pic").css( "width", "60%" );
+    	} // End of function that we want to be executed 
+    	  // when data is succesfully retrieved
+
+	} 	// End of object that was passed into jQuery's ajax() function call
+
+	); 	// End of ajax() function call
+
+} 		// End of definition for the fetchGif() function
+
